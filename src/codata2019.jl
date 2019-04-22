d1c0f2ba81439d37fe3f81c64f39911f3c9fc99f
Laurent G. OSSBSS AMIS UCC <27735554+LaGuer@users.noreply.github.com>
2019-04-22 04:00:42 -0700

    Update codata2019.jl

--- src/codata2019.jl
+++ src/codata2019.jl
@@ -1,9 +1,9 @@
 module CODATA2019
 
-using PhysicalConstants, Measurements, Unitful
+using PhysicalConstant, Measurements, Unitful
 
 import Unitful: Ω, A, C, F, J, kg, K, m, mol, N, Pa, s, T
-import PhysicalConstants: @constant, @derived_constant
+import PhysicalConstant: @constant, @derived_constant
 
 @constant(FineStructureConstant, α, "Fine-structure constant", 7.297_352_5664e-3,
           BigFloat(72_973_525_664)/BigFloat(10_000_000_000_000), Unitful.NoUnits,
