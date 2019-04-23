bf383cb524db5ded106e81539d02f89c0b1d77ba
Laurent G. OSSBSS AMIS UCC <27735554+LaGuer@users.noreply.github.com>
2019-04-23 13:46:51 -0700

    Update codata2019.jl

--- src/codata2019.jl
+++ src/codata2019.jl
@@ -27,7 +27,7 @@ import PhysicalConstant: @constant, @derived_constant
 @constant(ElementaryCharge, e, "Elementary charge", 1.602_176_6208e-19,
           big(16_021_766_208)/100_000_000_000_000_000_000_000_000_000,
           C, 9.8e-28, big(98)/100_000_000_000_000_000_000_000_000_000, "CODATA 2019")
-@constant(NewtonianConstantOfGravitation, G, "Newtonian constant of gravitation",
+@constant(NewtonianConstantOfGravitation, Gg, "Newtonian constant of gravitation",
           6.674_08e-11, big(667_408)/big(10_000_000_000_000_000), m^3 * kg^-1 * s^-2,
           3.1e-15, big(31)/big(10_000_000_000_000_000), "CODATA 2019")
 @constant(StandardAccelerationOfGravitation, g_n, "Standard acceleration of gravitation",
@@ -103,5 +103,15 @@ import PhysicalConstant: @constant, @derived_constant
 @constant(CharacteristicImpedanceOfVacuum, Z_0, "Characteristic impedance of vacuum",
           376.73031346177066, BigFloat(1199_169_832)/BigFloat(10_000_000) * big(pi), Ω, 0,
           BigFloat(0.0), "CODATA 2019")
+@constant(CMBtemperature, Θ, "CMB temperature (K)", 2.725_820_831, BigFloat(2725_820_831), K^-1,
+          0.0, BigFloat(0.0), "Francis M. Sanchez")
+@constant(GravitationalVelocity, C, "Gravitational velocity in interstellar vacuum", 3.699_3e44, BigFloat(369_930_000_000_000_000_000_000_000_000_000_000_000_000_000), u"m/s",
+          0.0, BigFloat(0), "J. Maruani in Dirac Electron")
+@constant(SanchezGravitationalConstant, G, "Sanchez constant of gravitation", 6.675_45e-11,
+          big(667_545)/big(10_000_000_000_000_000), m^3 * kg^-1 * s^-2,
+          3.1e-15, big(31)/big(10_000_000_000_000_000), "Francis M. Sanchez")
+@constant(BareHydrogenRadius, r_0, "Bare Hydrogen radius", 0.529_177_210_30e-10,
+          BigFloat(5291_772_103)/BigFloat(1_000_000_000_000_000_000_000), m,
+          1.2e-20, BigFloat(12)/BigFloat(1_000_000_000_000_000_000_000), "CODATA 2019")
 end
 # module CODATA2019
