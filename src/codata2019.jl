2f1cd91a84229659e4952a6cbd8c61b3211d6680
Laurent G. OSSBSS AMIS UCC <27735554+LaGuer@users.noreply.github.com>
2019-04-24 13:54:17 -0700

    Update codata2019.jl

--- src/codata2019.jl
+++ src/codata2019.jl
@@ -27,7 +27,7 @@ import PhysicalConstant: @constant, @derived_constant
 @constant(MagneticConstant, µ_0, "Magnetic constant", 1.2566370614359173e-6,
           4*big(pi)/BigFloat(10_000_000), N * A^-2, 0.0, BigFloat(0.0),
           "CODATA 2019")
-@constant(ElectricConstant, ε_0, "Electric constant Permittivity", 8.854187817620389e-12,
+@constant(ElectricConstant, ε_0, "Electric Constant Permittivity", 8.854187817620389e-12,
           inv(ustrip(big(µ_0)) * ustrip(big(c_0))^2), F * m^-1,
           0.0, BigFloat(0.0), "CODATA 2019")
 @constant(ElementaryCharge, e, "Elementary charge", 1.602_176_6208e-19,
@@ -53,9 +53,9 @@ import PhysicalConstant: @constant, @derived_constant
 @derived_constant(PlanckConstantOver2pi, ħ, "Planck constant over 2pi",
                   1.0545718001391127e-34, ustrip(big(h))/(2 * big(pi)), J * s,
                   measurement(h)/2pi, measurement(BigFloat, h)/(2 * big(pi)), "CODATA 2019")
-#@derived_constant(RedComptonElectronWavlgth, ƛ_e, "Reduced Compton Electron Wavelength",
-#                  1.380_648_52e-23, ustrip(big(ħ))/(c_0 * big(m_e)), Unitful.NoUnits,
-#                  measurement(ħ)/(c_0 * m_e), measurement(BigFloat, ħ)/(c_0 * big(m_e)), "CODATA 2019")
+@constant(RedComptonElectrWavlgth, ƛe, "Reduced Compton Electron Wavelength", 1.380_648_52e-23,
+          ustrip(big(ħ))/(c_0 * big(m_e)), Unitful.NoUnits,
+          0.0, BigFloat(0.0), "CODATA 2019")
 @constant(BoltzmannConstant, k_B, "Boltzmann constant", 1.380_648_52e-23,
           BigFloat(138_064_852)/BigFloat(10_000_000_000_000_000_000_000_000_000_000), J * K^-1,
           7.9e-30, BigFloat(79)/BigFloat(10_000_000_000_000_000_000_000_000_000_000), "CODATA 2019")
